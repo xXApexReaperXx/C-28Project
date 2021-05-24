155771557a7c55aab863a2eb7ecbc6aac4b3b41b
xXApexReaperXx <74620048+xXApexReaperXx@users.noreply.github.com>
2021-05-24 19:54:10 +0100

    Add files via upload

---
+++ js/tree.js
@@ -0,0 +1,28 @@
+class tree
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=450;
+		this.dustbinHeight=600;
+		this.wallThickness=10;
+		
+		this.image=loadImage("images/tree.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.treeWidth, this.treeThickness, {isStatic:true})
+		World.add(world,this.bottomBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			pop()
+			
+	}
+
+}
